b56709961761817e5a29574449582e9f2ae53cb3
Derek <schilke.60@gmail.com>
2018-05-08 13:55:28 -0700

    choosing random fractions

--- Oracle/oracle.py
+++ Oracle/oracle.py
@@ -4,83 +4,93 @@ from random import Random
 from Functions.polynomial import Polynomial
 
 from mpmath import floor, ceil, nint
-from sympy import Rational, sqrt
+from sympy import Rational, sqrt, S
 
 class Oracle(Random):
-	"""
-	Oracle.py - This is the object that knows how to deal with numbers
-	"""
+    """
+    Oracle.py - This is the object that knows how to deal with numbers
+    """
 
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-	# Random Number Generating Functions
+    # Random Number Generating Functions
 
-	def randint_nonmultiple(self,min,max,nonmultiple):
-		number = self.randint(min,max)
-		while number % nonmultiple == 0:
-			number = self.randint(min,max)
-		return number
+    def randint_nonmultiple(self,min,max,nonmultiple):
+        if nonmultiple == 1:
+            raise ValueError
+        number = self.randint(min,max)
+        while number % nonmultiple == 0:
+            number = self.randint(min,max)
+        return number
 
-	def randint_series(self,length,min,max,banned=[]):
-		series = []
-		while len(series) < length:
-			new = self.randint(min,max)
-			while new in banned:
-				new = self.randint(min,max)
-			series.append(new)
-		return series
+    def randfrac_complexity(self,dmin,dmax,nmin,nmax):
+        if dmin == 1:
+            raise ValueError
+        denominator = self.randint(dmin,dmax)
+        numerator = self.randint_nonmultiple(nmin,nmax,denominator)
+        x = S(str(numerator)+'/'+str(denominator))
+        return x
 
-	def random_polynomial(self,degree,number_of_terms,c_min,c_max,banned=[0]):
-		if number_of_terms < 2:
-			raise ValueError
-		the_coefficients = self.randint_series(number_of_terms-1,c_min,c_max,banned)
-		the_coefficients.extend([0 for x in range(degree - number_of_terms + 1)])
-		self.shuffle(the_coefficients)
-		the_coefficients.append(self.randint(c_min,c_max))
-		return Polynomial(the_coefficients)
+    def randint_series(self,length,min,max,banned=[]):
+        series = []
+        while len(series) < length:
+            new = self.randint(min,max)
+            while new in banned:
+                new = self.randint(min,max)
+            series.append(new)
+        return series
+
+    def random_polynomial(self,degree,number_of_terms,c_min,c_max,banned=[0]):
+        if number_of_terms < 2:
+            raise ValueError
+        the_coefficients = self.randint_series(number_of_terms-1,c_min,c_max,banned)
+        the_coefficients.extend([0 for x in range(degree - number_of_terms + 1)])
+        self.shuffle(the_coefficients)
+        the_coefficients.append(self.randint(c_min,c_max))
+        return Polynomial(the_coefficients)
 
 
-	# Number Theory Functions
+    # Number Theory Functions
 
-	def round(self,decimal,decimal_places):
-		enlarged = decimal * 10 ** (decimal_places)
-		if enlarged - floor(enlarged) == 0.5:
-			enlarged = ceil(enlarged)
-		else:
-			enlarged = nint(enlarged)
-		return enlarged / 10.0 ** decimal_places
+    def round(self,decimal,decimal_places):
+        enlarged = decimal * 10 ** (decimal_places)
+        if enlarged - floor(enlarged) == 0.5:
+            enlarged = ceil(enlarged)
+        else:
+            enlarged = nint(enlarged)
+        return enlarged / 10.0 ** decimal_places
 
-	def babylonian_root(self,radicand,guess):
-		'''Given a radicand and a guess, produce a better guess'''
-		return (guess + radicand / guess) / 2
+    def babylonian_root(self,radicand,guess):
+        '''Given a radicand and a guess, produce a better guess'''
+        return (guess + radicand / guess) / 2
 
-	def square_root_guess(self,radicand,number_type,precision):
-		return {
-			NumberType.RATIONAL:
-				Rational(sqrt(radicand).evalf(precision)).limit_denominator(precision),
-			NumberType.INTEGER:
-				sqrt(radicand).evalf(0),
-			NumberType.REAL:
-				self.round(sqrt(radicand).evalf(precision+2),precision)
-		}[number_type]
+    def square_root_guess(self,radicand,number_type,precision):
+        return {
+            NumberType.RATIONAL:
+                Rational(sqrt(radicand).evalf(precision)).limit_denominator(precision),
+            NumberType.INTEGER:
+                sqrt(radicand).evalf(0),
+            NumberType.REAL:
+                self.round(sqrt(radicand).evalf(precision+2),precision)
+        }[number_type]
 
-	def is_square(self,number):
-		'''Determine if the number is a perfect square'''
-		if number == 1:
-			return True
-		elif number < 4:
-			return False
-		x = number // 2
-		seen = set([x])
-		while x * x != number:
-			x = (x + (number // x)) // 2
-			if x in seen:
-				return False
-			seen.add(x)
-		return True
+    def is_square(self,number):
+        '''Determine if the number is a perfect square'''
+        if number == 1:
+            return True
+        elif number < 4:
+            return False
+        x = number // 2
+        seen = set([x])
+        while x * x != number:
+            x = (x + (number // x)) // 2
+            if x in seen:
+                return False
+            seen.add(x)
+        return True
 
 class NumberType(Enum):
-	RATIONAL = 'R'
-	INTEGER = 'Z'
-	REAL = 'Q'
+    RATIONAL = 'Q'
+    INTEGER = 'Z'
+    REAL = 'R'
--- Tests/test_oracle.py
+++ Tests/test_oracle.py
@@ -10,52 +10,58 @@ test_oracle = Oracle()
 class OracleTest(unittest.TestCase):
 
 
-	# Number Theory
+    # Number Theory
 
-	## Rounding
+    ## Rounding
 
-	def test_round_down(self):
-		self.assertEqual(test_oracle.round(3.141592,2),3.14)
+    def test_round_down(self):
+        self.assertEqual(test_oracle.round(3.141592,2),3.14)
 
-	def test_round_up(self):
-		self.assertEqual(test_oracle.round(3.141592,4),3.1416)
+    def test_round_up(self):
+        self.assertEqual(test_oracle.round(3.141592,4),3.1416)
 
-	## Perfect Squares
-	def test_is_square_pass(self):
-		for x in range(1234567891011121314151617181920,1234567891011121314151617181950):
-			self.assertTrue(test_oracle.is_square(x**2))
+    ## Perfect Squares
+    def test_is_square_pass(self):
+        for x in range(1234567891011121314151617181920,1234567891011121314151617181950):
+            self.assertTrue(test_oracle.is_square(x**2))
 
-	def test_is_square_fail(self):
-		self.assertFalse(test_oracle.is_square(152415789666209426002111556165263283035677490))
+    def test_is_square_fail(self):
+        self.assertFalse(test_oracle.is_square(152415789666209426002111556165263283035677490))
 
-	def test_is_square_negative(self):
-		self.assertFalse(test_oracle.is_square(-1))
+    def test_is_square_negative(self):
+        self.assertFalse(test_oracle.is_square(-1))
 
-	def test_square_root_one(self):
-		self.assertTrue(test_oracle.is_square(1))
+    def test_square_root_one(self):
+        self.assertTrue(test_oracle.is_square(1))
 
-	def test_square_root_primes(self):
-		primes = [2,3,5,7,11,13,17,23]
-		for x in primes:
-			self.assertFalse(test_oracle.is_square(x))
+    def test_square_root_primes(self):
+        primes = [2,3,5,7,11,13,17,23]
+        for x in primes:
+            self.assertFalse(test_oracle.is_square(x))
 
-	def test_randint_series_length(self):
-		for x in range(10):
-			result = test_oracle.randint_series(x,1,10)
-			self.assertEqual(len(result),x)
+    def test_randint_series_length(self):
+        for x in range(10):
+            result = test_oracle.randint_series(x,1,10)
+            self.assertEqual(len(result),x)
 
-	## Babylonian Roots
-	def test_babylonian_root(self):
-		self.assertEqual(test_oracle.babylonian_root(3,Fraction(7,4)),Fraction(97,56))
+    ## Babylonian Roots
+    def test_babylonian_root(self):
+        self.assertEqual(test_oracle.babylonian_root(3,Fraction(7,4)),Fraction(97,56))
 
-	def test_square_root_guess_rational(self):
-		result = test_oracle.square_root_guess(3,NumberType.RATIONAL,5)
-		self.assertEqual(Rational(7,4),result)
+    def test_square_root_guess_rational(self):
+        result = test_oracle.square_root_guess(3,NumberType.RATIONAL,5)
+        self.assertEqual(Rational(7,4),result)
 
-	def test_square_root_guess_integer(self):
-		result = test_oracle.square_root_guess(50,NumberType.INTEGER,66)
-		self.assertEqual(result,7)
+    def test_square_root_guess_integer(self):
+        result = test_oracle.square_root_guess(50,NumberType.INTEGER,66)
+        self.assertEqual(result,7)
 
-	def test_square_root_guess_real(self):
-		result = test_oracle.square_root_guess(5,NumberType.REAL,2)
-		self.assertEqual(result,2.24)
+    def test_square_root_guess_real(self):
+        result = test_oracle.square_root_guess(5,NumberType.REAL,2)
+        self.assertEqual(result,2.24)
+
+    ## Non-Multiple Generation
+    def test_nonmultiple_of_one_is_null(self):
+        with self.assertRaises(ValueError):
+            test_oracle.randint_nonmultiple(2,3,1)
+
